64e8acc211674f1e0c8e431a465ea62dedc3909c
arct297 <siniov.arseniy@gmail.com>
2024-12-13 11:50:45 +0500

    1st task is done

--- main.go
+++ main.go
@@ -6,30 +6,61 @@ import (
 	"net/http"
 )
 
-type Message struct {
-	Text string `json:"text"`
+type Request struct {
+	Message string `json:"message"`
 }
 
-func getMessage(w http.ResponseWriter, r *http.Request) {
-	msg := Message{Text: "Hello, world!"}
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func returnJSONError(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(msg)
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(Response{
+		Status:  "fail",
+		Message: message,
+	})
 }
 
-func postMessage(w http.ResponseWriter, r *http.Request) {
-	var msg Message
-	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+func operateMessage(w http.ResponseWriter, r *http.Request) {
+	var request Request
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		returnJSONError(w, "Incorrect JSON message", http.StatusBadRequest)
 		return
 	}
 
+	if request.Message == "" {
+		returnJSONError(w, "Incorrect JSON message", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Received message: %s", request.Message)
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(msg)
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(Response{
+		Status:  "success",
+		Message: "data has been successfully received",
+	})
+}
+
+func handleMessage(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		operateMessage(w, r)
+	case "POST":
+		operateMessage(w, r)
+	default:
+		http.Error(w, "Unsupported HTTP method", http.StatusMethodNotAllowed)
+	}
 }
 
 func main() {
-	http.HandleFunc("/message", getMessage)
-	// http.HandleFunc("/message", postMessage)
+	http.HandleFunc("/message", handleMessage)
 	log.Println("Server starting on http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import requests
 
-r = requests.get("http://127.0.0.1:8080/message")
+# r = requests.get("http://127.0.0.1:8080/message")
 
-print(r.status_code)
-print(r.text)
+
+def test_post():
+    url = "http://127.0.0.1:8080/message"
+    json = {
+        "message1" : "ddsds",
+    }
+
+    r = requests.post(url, json = json)
+
+    print(r.status_code)
+    print(r.text)
+
+
+if __name__ == "__main__":
+    test_post()
